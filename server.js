9e2eafa94fc9eba31c5b447e7791ec5fb476af44
Ritesh Sharma <rs3907367@gmail.com>
2026-04-20 12:26:16 +0530

    Enhance CORS configuration in server.js
    Added CORS methods and allowed headers configuration.

--- server.js
+++ server.js
@@ -133,10 +133,10 @@ app.use(cors({
       return callback(new Error('Not allowed by CORS'));
     }
   },
-  credentials: true
+  credentials: true,
+  methods: ['GET','POST','PUT','DELETE','PATCH','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
 }));
-
-app.options('*', cors());
 
 app.use(express.json({ limit: '15mb' }));
 app.use(express.urlencoded({ extended: true, limit: '15mb' }));
