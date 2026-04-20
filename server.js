06e0f6ce8d51cf13970146cd381c42fcdcec5c68
Ritesh Sharma <rs3907367@gmail.com>
2026-04-20 14:19:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -106,6 +106,7 @@ app.use(helmet({ crossOriginResourcePolicy: { policy: 'cross-origin' } }));
 app.use(cors({
   origin: [
     'http://localhost:5173',
+    'http://127.0.0.1:5173',
     'https://pixelforge-ritesh-sharma.vercel.app'
   ],
   credentials: true
